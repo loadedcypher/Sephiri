97866d4db26114c366f61783b4604d152fd6bbfc
loadedcypher <dambuzatumo@gmail.com>
2024-04-28 13:44:50 +0200

    wrote encryption and decryption api endpoints

--- sephiri/src/main/java/com/buza/sephiri/MyController.java
+++ sephiri/src/main/java/com/buza/sephiri/MyController.java
@@ -24,5 +24,25 @@ public class MyController {
     public String readFile(@RequestParam String fileName) {
         return Secure.readFile(fileName);
     }
+
+    //API endpoint to read the contents of a file given a filename as a parameter.
+    @GetMapping("/api/writeFile")
+    public void writeFile(@RequestParam String fileName, @RequestParam String location, @RequestParam String fileContents) {
+        Secure.writeFile(fileName, location,  fileContents);
+    }
+
+    // API endpoint to just encrypt a file's contents given a key
+
+    @GetMapping("/api/encryptFile")
+    public String encryptFile(@RequestParam String fileName, @RequestParam int key) {
+        return Secure.encryptFile(fileName, key);
+    }
+
+    @GetMapping("/api/decryptFile")
+    public String decryptFile(@RequestParam String fileName, @RequestParam int key) {
+        return Secure.decryptFile(fileName, key);
+    }
+
+
 }
 
